0a2419d97fbc622149d8d3f0de10401c94812a3b
bamikefa-adewale <bamikefaadewale@gmail.com>
2022-12-11 06:35:47 +0100

    Code base refactory

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,25 @@
 import { useContext } from "react";
 import PostItems from "../components/posts/PostItems";
+import AuthContext from "../context/auth/authContext";
 import PostContext from "../context/postContext/postContext";
 
 const Dashboard = () => {
   const postContext = useContext(PostContext);
+  const usersContext = useContext(AuthContext);
 
   const { posts, loading } = postContext;
   if (loading) {
     return <h1>Loading</h1>;
   }
   return (
-    <div className="mx-auto container py-6 grid md:grid-cols-3 grid-cols-1 gap-4 p-4">
-      {posts && posts.map((post) => <PostItems key={post.id} post={post} />)}
-    </div>
+    <main className="mx-auto container py-6  px-4">
+      <h2 className=" text-right text-3xl my-4 text-blue-500 font-semibold">
+        {usersContext.users?.username}
+      </h2>
+      <div className=" grid md:grid-cols-3 grid-cols-1 gap-4">
+        {posts && posts.map((post) => <PostItems key={post.id} post={post} />)}
+      </div>
+    </main>
   );
 };
 
